0c5acc6748ef74029d117a617ff10882e6e5452c
Serdyukov Alexander <30432511+SerdyukovAlexander@users.noreply.github.com>
2024-12-05 13:26:57 +0500

    7 модуль, Сердюков ПрИ-201

--- task01/src/com/example/task01/Task01Main.java
+++ task01/src/com/example/task01/Task01Main.java
@@ -1,21 +1,30 @@
 package com.example.task01;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 
-public class Task01Main {
-    public static void main(String[] args) throws IOException {
-        //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
-        // например вот так:
-
-        /*
+public class Task01Main
+{
+    public static void main(String[] args) throws IOException
+    {
         System.out.println(checkSumOfStream(new ByteArrayInputStream(new byte[]{0x33, 0x45, 0x01})));
-        */
-
     }
 
-    public static int checkSumOfStream(InputStream inputStream) throws IOException {
-        // your implementation here
-        return 0;
+    public static int checkSumOfStream(InputStream inputStream) throws IOException
+    {
+        if (inputStream == null){
+            throw  new IllegalArgumentException();
+        }
+
+        int b = inputStream.read();
+        int checkSum = 0;
+
+        while (b != -1) {
+
+            checkSum = Integer.rotateLeft(checkSum, 1) ^ b;
+            b = inputStream.read();
+        }
+        return checkSum;
     }
 }
--- task02/src/com/example/task02/Task02Main.java
+++ task02/src/com/example/task02/Task02Main.java
@@ -1,13 +1,30 @@
 package com.example.task02;
 
 import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
 
-public class Task02Main {
-    public static void main(String[] args) throws IOException {
-        // чтобы протестировать свое решение, вам нужно:
-        // - направить файл input.test в стандартный ввод программы (в настройках запуска программы в IDE или в консоли)
-        // - направить стандартный вывод программы в файл output.test
-        // - запустить программу
-        // - и сравнить получившийся файл output.test с expected.test
+public class Task02Main
+{
+    public static void main(String[] args) throws IOException
+    {
+        InputStream inputStream = System.in;
+        OutputStream outputStream = System.out;
+        int previous = inputStream.read();
+        int current = inputStream.read();
+
+        while (previous != -1){
+
+            if (current == 10 && previous == 13){
+                previous = current;
+                current = inputStream.read();
+            }
+
+            outputStream.write(previous);
+
+            previous = current;
+            current = inputStream.read();
+        }
+        outputStream.flush();
     }
 }
